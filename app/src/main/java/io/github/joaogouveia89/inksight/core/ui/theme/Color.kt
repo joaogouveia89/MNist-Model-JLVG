2fe6954e1cd491f2bd0dcfe9e9293158233c9694
joaogouveia89 <j.lucas.gouveia@gmail.com>
2026-02-05 13:14:32 +0000

    IDE linter fixes

--- app/src/main/java/io/github/joaogouveia89/inksight/core/ui/theme/Color.kt
+++ app/src/main/java/io/github/joaogouveia89/inksight/core/ui/theme/Color.kt
@@ -20,11 +20,3 @@ val WarningOrange = Color(0xFFFF9800)
 val RedAlert = ErrorRed
 val Orange = WarningOrange
 val Yellow = AccentYellow
-
-// Standard Palette fallback
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
