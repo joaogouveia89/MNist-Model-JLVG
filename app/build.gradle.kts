377d2885362cce20cb8c77332e5fefc94790a8b3
joaogouveia89 <j.lucas.gouveia@gmail.com>
2026-02-05 12:12:15 +0000

    updated version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "io.github.joaogouveia89.inksight"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 5
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
